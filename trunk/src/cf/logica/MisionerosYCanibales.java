0ee8c502de6e9543c7c7b943209dec64bb0c53a2
luismoyanorufo <luismoyanorufo@cfe8ca6a-f0cd-11de-a89a-43a960051dd3>
2009-12-28 21:29:29 +0000

    Primera versión del CasinoFantasma. Ni caso de la gui por el momento, es de un jueguecillo que tenia y he hecho copia-pega.  Mira en cf.logica estan los mini-juegos

---
+++ trunk/src/cf/logica/MisionerosYCanibales.java
@@ -0,0 +1,19 @@
+package cf.logica;
+
+/**
+ *
+ * @author luigi
+ */
+public class MisionerosYCanibales extends Minijuego {
+
+    @Override
+    public boolean estadoObjetivo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean hazMovimiento(int movimiento) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
